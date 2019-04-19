dc36da35ac80bd24ff19165dec087d016a71a848
cluder <cluder@users.noreply.github.com>
2019-04-20 01:07:27 +0200

    change recursion level with up/down keys
    + changed colors

--- src/example/fractals/sierpinski/SierpinskiTriangle.java
+++ src/example/fractals/sierpinski/SierpinskiTriangle.java
@@ -37,6 +37,7 @@ public class SierpinskiTriangle extends PApplet {
 		t.a = new PVector(width / 2, pad);
 		t.b = new PVector(width - pad, height - pad);
 		t.c = new PVector(pad, height - pad);
+		background(255, 255);
 	}
 
 	Triangle t = new Triangle();
@@ -50,12 +51,18 @@ public class SierpinskiTriangle extends PApplet {
 
 		fill(0);
 		t.draw();
-		subdivide(t);
+		subdivide(t, 0);
+
+		textSize(20);
+		textAlign(PApplet.LEFT, PApplet.CENTER);
+		text("Levels:" + maxLevel, 20, 20);
 	}
 
 	boolean isFirst = true;
+	int maxLevel = 7;
 
-	public void subdivide(Triangle t) {
+	public void subdivide(Triangle t, int level) {
+		level++;
 		// right
 		Triangle sub = new Triangle();
 		sub.a = new PVector();
@@ -63,7 +70,7 @@ public class SierpinskiTriangle extends PApplet {
 		sub.a.y = (t.a.y + t.b.y) / 2f;
 
 		// min triangle length - recursion abort
-		if (abs(t.a.x - t.b.x) < 8) {
+		if (level >= maxLevel) {
 			return;
 		}
 
@@ -77,27 +84,61 @@ public class SierpinskiTriangle extends PApplet {
 		sub.c.x = (t.c.x + t.a.x) / 2f;
 		sub.c.y = (t.c.y + t.a.y) / 2f;
 
-		fill(255, 255);
+		final int color = 255 - level * 25;
+//		switch (level) {
+//		case 1:
+//			fill(color, color, 0);
+//			break;
+//		case 2:
+//			fill(0, color, color);
+//			break;
+//		case 3:
+//			fill(color, 0, color);
+//			break;
+//		case 4:
+//			fill(color, color, 0);
+//			break;
+//		case 5:
+//			fill(0, color, color);
+//			break;
+//		case 6:
+//			fill(color, 0, color);
+//			break;
+//		default:
+//			fill(color, color, color);
+//			break;
+//		}
+		fill(color, color, color);
 		sub.draw();
 
 		Triangle top = new Triangle();
 		top.a = t.a;
 		top.b = sub.a;
 		top.c = sub.c;
-		subdivide(top);
+		subdivide(top, level);
 
 		Triangle left = new Triangle();
 		left.a = sub.a;
 		left.b = t.b;
 		left.c = sub.b;
-		subdivide(left);
+		subdivide(left, level);
 
 		Triangle right = new Triangle();
 		right.a = sub.c;
 		right.b = sub.b;
 		right.c = t.c;
-		subdivide(right);
+		subdivide(right, level);
 
 //		subdivide(sub);
+	}
+
+	@Override
+	public void keyPressed() {
+		if (keyPressed && keyCode == PApplet.UP) {
+			maxLevel++;
+		}
+		if (keyPressed && keyCode == PApplet.DOWN) {
+			maxLevel--;
+		}
 	}
 }
